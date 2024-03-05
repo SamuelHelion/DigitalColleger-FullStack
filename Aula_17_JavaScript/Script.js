b9afb8b30ad8c6c6580a8eb61789c7b54eac2810
SAMUEL HELION DUARTE DE MENEZES <SAMUEL0633@DIGITALCOLLEGE.LOCAL>
2024-03-04 21:25:32 -0300

    Aula 17

---
+++ Aula_17_JavaScript/Script.js
@@ -0,0 +1,51 @@
+//Criação de programa para iterar valores de 0 a 100 e condição para que seja separado
+//Números pares dos números impares.
+
+//For e condição
+
+// for(let contador = 0; contador <= 100; contador++){
+    
+//     if(contador % 2 === 0);
+//     console.log(`Numeros pares são ${contador}`)
+    
+//     else{
+//         console.log(`Numeros Impares são ${contador}`)  
+//     }
+// }
+
+
+//Criar um programa que itere de 0 s 100 e que ele diga se for multiplo de 3
+// mostrar na tela que é multiplo de 3 e se for multiplo de 5, mostrar na tela 
+//que é multiplo 5.
+
+// for(let contador = 0; contador <= 100; contador++){
+
+//     if(contador % 3 === 0 && contador % 5 === 0);
+//         console.log(`Numero multiplos de 3 e 5 são ${contador}`);
+
+// }
+
+/*
+Desenvolva um algoritmo que leia a altura de 15 pessoas,
+este programa devera calcular e mostrar
+
+a. A menor altura do grupo
+b. A maior altura do grupo
+*/
+
+
+let MenorAltura = 0;
+let MaiorAltura = 0;
+
+const somasAlturas = MenorAltura + MaiorAltura
+
+for(let contador = 1; contador <= 5; contador++){
+
+var nome = prompt("Qual seu nome")
+var altura = parseFloat(prompt("Qual a sua altura"))
+
+if(altura == MaiorAltura)
+    console.log(`$`)
+}
+
+//Finalizar o Problema a cima.
