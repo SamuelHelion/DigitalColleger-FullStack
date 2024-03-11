7d4e533671bf563d210f4e7c923f0c74ad6b4623
SAMUEL HELION DUARTE DE MENEZES <SAMUEL0633@DIGITALCOLLEGE.LOCAL>
2024-03-11 19:42:47 -0300

    Aula 18

---
+++ Aula_18/Exerc_01.js
@@ -0,0 +1,42 @@
+//CRIE UMA LISTA COM 3 MODELOS DE CARROS
+// CADA CARRO DEVE ARMAZENAR COR, MAR E MODELO.
+//UTILIZE ARRAY E OBJETOS.
+
+const carro_1 = {
+    cor: "vermelho",
+    marca: "toyota",
+    modelo: "Corolla",
+    proprietario: [ 
+        {
+            nome: "Luckas",
+            Idade: 22,
+        },
+    ]    
+};
+
+const carro_2 = {
+    cor: "azul",
+    Marca: "chevrolet",
+    modelo: "Onix",
+    proprietario: [ 
+        {
+            nome: "Samuel",
+            Idade: 32,
+        },
+    ]
+
+};
+
+const carro_3 = {
+    cor: "verde",
+    marca: "Fiat",
+    modelo: "Mobi",
+    proprietario: [ 
+        {
+            nome: "Leandro",
+            Idade: 45,
+        },
+    ]
+};
+
+console.log(carro_1)
